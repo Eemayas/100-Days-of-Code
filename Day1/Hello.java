2fd0e9cb5d7a360db87d2a096f24548ab1fd9cf8
Ëemayas <prashantmanandhar2002@gmail.com>
2024-01-11 18:26:52 +0545

    [Day 2] Data Types

--- Day1/Hello.java
+++ Day1/Hello.java
@@ -1,7 +1,16 @@
-package Day1;
-
 class Hello {
     public static void main(String a[]) {
-        System.out.print("Hello World");
+        byte i = 3;
+        int by = 127;
+        short sh = 558;
+        long lg = 5854l; // l is required at the end of the code to specify it is long type variable
+
+        float ft = 7.2f;
+        double db = 7.2;
+
+        char c = 'k';
+
+        boolean b = true;
+
     }
 }
