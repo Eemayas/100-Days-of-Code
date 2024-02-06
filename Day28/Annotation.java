7930d0ef5b75363918da5e696dede04badcb6748
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-06 19:36:29 +0545

    [Day 28] Annotation

---
+++ Day28/Annotation.java
@@ -0,0 +1,26 @@
+
+@Deprecated
+class A {
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("in show A");
+    }
+}
+
+class B extends A {
+    @Override
+    // public void showTheDataWhichBelongToThisClass()
+    public void showTheDataWhichBelongsToThisClass()
+
+    {
+        System.out.println("in show B");
+    }
+}
+
+public class Annotation {
+    public static void main(String[] args) {
+
+        B obj = new B();
+        obj.showTheDataWhichBelongsToThisClass();
+
+    }
+}
