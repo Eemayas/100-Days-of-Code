9b998058e1816f678802bf2a5324a0f12881bbe1
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-07 12:17:13 +0545

    [Day 29] FunctionalInterface

---
+++ Day29/FunctionalInterfaces.java
@@ -0,0 +1,27 @@
+
+@FunctionalInterface
+interface A {
+    void show();
+    // void run();
+}
+// class B implements A
+// {
+// public void show()
+// {
+// System.out.println("in Show");
+// }
+// }
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+
+        A obj = new A() {
+            public void show() {
+                System.out.println("in Show");
+            }
+        };
+        // A obj=new A();
+        // A obj=new B();
+        obj.show();
+    }
+}
