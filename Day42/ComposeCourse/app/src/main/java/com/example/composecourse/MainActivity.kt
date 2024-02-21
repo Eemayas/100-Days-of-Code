9635d50949a07f912b87dd6184fd4f916ab6fb5e
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-21 19:38:17 +0545

    [Day 43] Modifiers

--- Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,33 +3,92 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(Modifier.background(Color.Green).fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                Text(text = "Hello Prashant")
-                Text(text = "Hello Manandhar")
-                Row {
-                    Text(text = "Hello Prashant")
-                    Text(text = "Hello Manandhar")
-                }  }
+            val painter = painterResource(id = R.drawable.photo);
+            val description = "Prashant";
+            val title = "Prashant Manandhar Photo";
+            ImageCard(painter = painter, contentDescriptor = description, title = title)
 
+
+//            Column(Modifier.background(Color.Green).fillMaxHeight().width(600.dp). requiredWidth(300.dp).padding(50.dp,0.dp
+//            ),) {
+//                Text(text = "Hello Prashant",Modifier
+//                        .offset(0.dp,20.dp))
+//                Text(text = "Hello Manandhar")
+//                Row {
+//                    Text(text = "Hello Prashant",Modifier
+//                        .offset(50.dp,20.dp))
+//                    Text(text = "Hello Manandhar")
+//                }  }
+
+        }
+    }
+
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescriptor: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxHeight(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.photo),
+                contentDescription =  "hello"
+//            painter=painter,
+//            contentDescriptor=contentDescriptor,
+//            contentScale= ContentScale.Crop
+            )
+           
+           
+           
+           
+        }
+        Box(
+            Modifier
+                .fillMaxHeight()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Text(
+                title, style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                )
+            )
         }
     }
 }
@@ -37,8 +96,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier
     )
 }
 
