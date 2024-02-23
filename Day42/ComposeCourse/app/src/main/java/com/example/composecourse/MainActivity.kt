7fce19a746413d7d2d0c08ba22b05f75424ac53c
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-23 20:09:16 +0545

    [Day 45] State

--- Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,71 +16,92 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecourse.ui.theme.ComposeCourseTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val fontFamily = FontFamily(
-            Font(R.font.opensans_bold),
-            Font(R.font.opensans_bolditalic),
-            Font(R.font.opensans_condensed_semibold),
-        )
         setContent {
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .background(
-                        Color(0xFF101010)
-                    )
+            Column(
+                Modifier.fillMaxSize()
             ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp,
-                            )
-                        ) {
-                            append("P")
-                        }
-                        append("rashant")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp,
-                            )
-                        ) {
-                            append("M")
-                        }
-                        append("anandhar")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily, fontWeight = FontWeight(500),
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+
+                ){
+                    color.value=it
+                }
+                Box(
+                    Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
                 )
             }
+
+
+//Day 44
+
+//        val fontFamily = FontFamily(
+//            Font(R.font.opensans_bold),
+//            Font(R.font.opensans_bolditalic),
+//            Font(R.font.opensans_condensed_semibold),
+//        )
+//        setContent {
+//            Box(
+//                Modifier
+//                    .fillMaxSize()
+//                    .background(
+//                        Color(0xFF101010)
+//                    )
+//            ) {
+//                Text(
+//                    text = buildAnnotatedString {
+//                        withStyle(
+//                            style = SpanStyle(
+//                                color = Color.Green,
+//                                fontSize = 50.sp,
+//                            )
+//                        ) {
+//                            append("P")
+//                        }
+//                        append("rashant")
+//                        withStyle(
+//                            style = SpanStyle(
+//                                color = Color.Green,
+//                                fontSize = 50.sp,
+//                            )
+//                        ) {
+//                            append("M")
+//                        }
+//                        append("anandhar")
+//                    },
+//                    color = Color.White,
+//                    fontSize = 30.sp,
+//                    fontFamily = fontFamily, fontWeight = FontWeight(500),
+//                    fontStyle = FontStyle.Italic,
+//                    textAlign = TextAlign.Center
+//                )
+//            }
 
 //Day 43
 //            val painter = painterResource(id = R.drawable.photo);
@@ -107,6 +130,32 @@ class MainActivity : ComponentActivity() {
 //                }  }
 
         }
+    }
+
+}
+
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor:(Color )->Unit
+) {
+
+
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(  Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            ))
+
+        }
+    ) {
+
     }
 
 }
