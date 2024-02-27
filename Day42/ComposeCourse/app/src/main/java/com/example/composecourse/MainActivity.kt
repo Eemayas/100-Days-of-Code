ee2d09051b62a0b96f724c754b337920a5284b75
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-27 20:04:47 +0545

    [Day 49] Simple Animation

--- Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,68 +16,112 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
+    private var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            var sizeState by remember { mutableStateOf(200.dp) }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+                animationSpec =
+//                keyframes {
+//                    durationMillis = 5000
+//                    sizeState at 0 with LinearEasing
+//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//                    sizeState * 2f at 5000
+//
+//                },
+                tween(durationMillis = 1000),
+                label = ""
+            )
 
-            val constraints = ConstraintSet {
-                val greenBox = createRefFor("greenbox")
-                val redBox = createRefFor("redbox")
-                val guideline=createGuidelineFromTop(0.5f)
-
-                constrain(greenBox) {
-                    top.linkTo(guideline)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-
+            //For Infinite Animation
+            val infiniteTransition = rememberInfiniteTransition(label = "");
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = InfiniteRepeatableSpec(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                ), label = ""
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .size(sizeState)
+                    .background(color),
+                contentAlignment = Alignment.Center,
+            ) {
+                Button(onClick = { sizeState += 50.dp }) {
+                    Text("Increase Size")
                 }
-                constrain(redBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    end.linkTo(parent.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.Packed)
             }
 
-            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-                Box(
-                    modifier = Modifier.background(Color.Green)
-                        .layoutId("greenbox")
-                )
-                Box(
-                    modifier = Modifier.background(Color.Red)
-                        .layoutId("redbox")
-                )
-            }
         }
+
+// Day 48
+//        setContent {
+//
+//            val constraints = ConstraintSet {
+//                val greenBox = createRefFor("greenbox")
+//                val redBox = createRefFor("redbox")
+//                val guideline=createGuidelineFromTop(0.5f)
+//
+//                constrain(greenBox) {
+//                    top.linkTo(guideline)
+//                    start.linkTo(parent.start)
+//                    width = Dimension.value(100.dp)
+//                    height = Dimension.value(100.dp)
+//
+//                }
+//                constrain(redBox) {
+//                    top.linkTo(parent.top)
+//                    start.linkTo(greenBox.end)
+//                    end.linkTo(parent.end)
+//                    width = Dimension.value(100.dp)
+//                    height = Dimension.value(100.dp)
+//                }
+//                createHorizontalChain(greenBox,redBox, chainStyle = ChainStyle.Packed)
+//            }
+//
+//            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+//                Box(
+//                    modifier = Modifier.background(Color.Green)
+//                        .layoutId("greenbox")
+//                )
+//                Box(
+//                    modifier = Modifier.background(Color.Red)
+//                        .layoutId("redbox")
+//                )
+//            }
+//        }
 
 // Day 47
 //// ListView using LazyColumn
