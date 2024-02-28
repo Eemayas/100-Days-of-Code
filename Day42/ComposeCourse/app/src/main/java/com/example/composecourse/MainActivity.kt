6344c12b7e1f4fed7217debcaaee188c0b60dcdf
Ëemayas <prashantmanandhar2002@gmail.com>
2024-02-28 20:12:00 +0545

    [Day 50] Animation (Circular Progress Bar)

--- Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ Day42/ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,12 +3,9 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.InfiniteRepeatableSpec
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -18,11 +15,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,10 +28,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecourse.ui.theme.ComposeCourseTheme
@@ -47,44 +49,54 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                animationSpec =
-//                keyframes {
-//                    durationMillis = 5000
-//                    sizeState at 0 with LinearEasing
-//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-//                    sizeState * 2f at 5000
-//
-//                },
-                tween(durationMillis = 1000),
-                label = ""
-            )
-
-            //For Infinite Animation
-            val infiniteTransition = rememberInfiniteTransition(label = "");
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = InfiniteRepeatableSpec(
-                    tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                ), label = ""
-            )
             Box(
-                modifier = Modifier
-                    .size(size)
-                    .size(sizeState)
-                    .background(color),
-                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Button(onClick = { sizeState += 50.dp }) {
-                    Text("Increase Size")
-                }
+                CircularProgressBar(percentage = 0.8f, number = 100)
             }
 
         }
+// Day 49
+//        setContent {
+//            var sizeState by remember { mutableStateOf(200.dp) }
+//            val size by animateDpAsState(
+//                targetValue = sizeState,
+//                animationSpec =
+////                keyframes {
+////                    durationMillis = 5000
+////                    sizeState at 0 with LinearEasing
+////                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+////                    sizeState * 2f at 5000
+////
+////                },
+//                tween(durationMillis = 1000),
+//                label = ""
+//            )
+//
+//            //For Infinite Animation
+//            val infiniteTransition = rememberInfiniteTransition(label = "");
+//            val color by infiniteTransition.animateColor(
+//                initialValue = Color.Red,
+//                targetValue = Color.Green,
+//                animationSpec = InfiniteRepeatableSpec(
+//                    tween(durationMillis = 2000),
+//                    repeatMode = RepeatMode.Reverse
+//                ), label = ""
+//            )
+//            Box(
+//                modifier = Modifier
+//                    .size(size)
+//                    .size(sizeState)
+//                    .background(color),
+//                contentAlignment = Alignment.Center,
+//            ) {
+//                Button(onClick = { sizeState += 50.dp }) {
+//                    Text("Increase Size")
+//                }
+//            }
+//
+//        }
 
 // Day 48
 //        setContent {
@@ -341,6 +353,57 @@ fun ColorBox(
     }
 
 }
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0,
+
+    ) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val currentPercentage =
+        animateFloatAsState(
+            targetValue = if (animationPlayed) percentage else 0f,
+            animationSpec = tween(
+                durationMillis = animationDuration,
+                delayMillis = animationDelay
+            )
+        )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
 
 @Composable
 fun ImageCard(
